965ef662f9b4b3827650464cdd756406727c3e56
Chet Russell <chet_dylan@outlook.com>
2022-07-26 10:04:29 -0800

    Formatted

--- gui.py
+++ gui.py
@@ -10,78 +10,77 @@ from main import orthorec
 from main import seg
 from main import calculate_gsd
 
-sg.theme('Default')
+sg.theme("Default")
 
 layout = [
     # Initial Folders
-    [
-        sg.Text("Image Folder:", font=("Helvetica", 15)),
-        sg.FolderBrowse(key='imgdir')
-    ],
-    [
-        sg.Text("Results Folder:", font=("Helvetica", 15)),
-        sg.FolderBrowse(key='dwndir')
-    ],
-    [sg.Text('')],
-    
+    [sg.Text("Image Folder:", font=("Helvetica", 15)), sg.FolderBrowse(key="imgdir")],
+    [sg.Text("Results Folder:", font=("Helvetica", 15)), sg.FolderBrowse(key="dwndir")],
+    [sg.Text("")],
     # Orthorectification options
-    [sg.Text("Orthorectification", font=("Helvetica", 20), key='ort')],
-    [   
+    [sg.Text("Orthorectification", font=("Helvetica", 20), key="ort")],
+    [
         sg.Text("Pixel Buffer (for glint masking):"),
-        sg.Combo([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],default_value=5,key='pb')
+        sg.Combo([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], default_value=5, key="pb"),
     ],
-    [   
+    [
         sg.Text("Quality:"),
-        sg.Combo(['ultra', 'high', 'medium', 'low', 'lowest'],default_value='high',key='q')
+        sg.Combo(
+            ["ultra", "high", "medium", "low", "lowest"], default_value="high", key="q"
+        ),
     ],
     [
         sg.Text("Crop:"),
-        sg.Combo([0, 1, 2, 3, 4, 5],default_value=0,key='crop'),
-        #sg.Text("This setting has the tendency to crop out water areas, use with caution.")
+        sg.Combo([0, 1, 2, 3, 4, 5], default_value=0, key="crop"),
+        # sg.Text("This setting has the tendency to crop out water areas, use with caution.")
     ],
-    [
-        sg.Text("Generate KMZ (Google Earth) render:"),
-        sg.Combo([True, False],default_value='False',key='kmz')
-    ],
+    # WORK IN PROGRESS
+    # [
+    #    sg.Text("Generate KMZ (Google Earth) render:"),
+    #    sg.Combo([True, False],default_value='False',key='kmz')
+    # ],
     [
         sg.Text("Feature-type algorithm:"),
-        sg.Combo(['sift', 'akaze', 'hahog', 'orb'],default_value='sift',key='ft'),
+        sg.Combo(["sift", "akaze", "hahog", "orb"], default_value="sift", key="ft"),
     ],
-
     # WORK IN PROGRESS
-    #[
+    # [
     #    sg.Text("Use EXIF:"),
     #    sg.Combo([True, False],default_value=True,key='exif')
-    #],
-
-    [sg.Button("Run Orthorectification Independently"), sg.Text("This will take a while. Be patient.")],
-    [sg.Text('')],
-
+    # ],
+    [
+        sg.Button("Run Orthorectification Independently"),
+    ],
+    [sg.Text("")],
     # Kelpomatic options
-    [sg.Text("Kelp Identification (Kelpomatic)", font=("Helvetica", 20), key='ki')],
+    [sg.Text("Kelp Identification (Kelpomatic)", font=("Helvetica", 20), key="ki")],
     [
         sg.Text("Orthomosaic File:"),
-        sg.FileBrowse(key='orthfile')
+        sg.FileBrowse(key="orthfile"),
+        sg.Text("Only required when running independently.", font=("Helvetica", 8)),
     ],
     [
         sg.Text("Results Folder:"),
-        sg.FolderBrowse(key='resdir')
-    ], 
-    [sg.Text('GSD:'), sg.InputText(size=(5, 1), key='gsd')], 
-
+        sg.FolderBrowse(key="resdir"),
+        sg.Text("Only required when running independently.", font=("Helvetica", 8)),
+    ],
+    [
+        sg.Text("GSD:"),
+        sg.InputText(size=(5, 1), key="gsd"),
+        sg.Text("Only required when running independently.", font=("Helvetica", 8)),
+    ],
     # WORK IN PROGRESS
-    #[
+    # [
     #    sg.Text("Species classification:"),
     #    sg.Combo([True, False],default_value='False',key='spec')
-    #],
-
+    # ],
     [sg.Button("Run Identification Independently")],
-    [sg.Button("Run All", font=("Helvetica", 20))]
+    [sg.Button("Run All", font=("Helvetica", 20))],
 ]
 
-window = sg.Window('Kelpy', layout,size=(700, 700))
-ort = window['ort']
-ki = window['ki']
+window = sg.Window("Kelpy", layout, size=(700, 700))
+ort = window["ort"]
+ki = window["ki"]
 
 while True:  # Event Loop
     event, values = window.read()
@@ -90,51 +89,78 @@ while True:  # Event Loop
         break
 
     # Running the glint mask generator and the ODM orthomosaic generator
-    if event == 'Run Orthorectification Independently':
-        if values['imgdir'] == '':
-            sg.popup('ERROR: No image folder selected.', title='ERROR')
+    if event == "Run Orthorectification Independently":
+        if values["imgdir"] == "":
+            sg.popup("ERROR: No image folder selected.", title="ERROR")
         else:
-            if values['dwndir'] == '':
-                sg.popup('ERROR: No results folder selected.', title='ERROR')
+            if values["dwndir"] == "":
+                sg.popup("ERROR: No results folder selected.", title="ERROR")
             else:
-                masker(values['imgdir'], values['pb'])
-                orthorec(values['imgdir'], values['dwndir'], values['q'], values['crop'], values['kmz'], values['ft'], False)
-                ort.update('Orthorectification: DONE')
+                masker(values["imgdir"], values["pb"])
+                orthorec(
+                    values["imgdir"],
+                    values["dwndir"],
+                    values["q"],
+                    values["crop"],
+                    values["kmz"],
+                    values["ft"],
+                    False,
+                )
+                ort.update("Orthorectification: DONE")
 
     # Running Kelpomatic
-    if event == 'Run Identification Independently':
-        if values['orthfile'] == '':
-            sg.popup('ERROR: No orthomosaic file selected.', title='ERROR')
-        else: 
-            if values['resdir'] == '':
-                sg.popup('ERROR: No results folder selected.', title='ERROR')
+    if event == "Run Identification Independently":
+        if values["orthfile"] == "":
+            sg.popup("ERROR: No orthomosaic file selected.", title="ERROR")
+        else:
+            if values["resdir"] == "":
+                sg.popup("ERROR: No results folder selected.", title="ERROR")
             else:
-                if values['gsd'] == '':
-                    sg.popup('ERROR: GSD value empty.', title='ERROR')
+                if values["gsd"] == "":
+                    sg.popup("ERROR: GSD value empty.", title="ERROR")
                 else:
                     try:
-                        value = float(values['gsd'])
-                        ki.update('Identification (Kelpomatic): DONE')
-                        sg.popup(seg(values['orthfile'], values['resdir'], value, False), title='results')
+                        value = float(values["gsd"])
+                        ki.update("Identification (Kelpomatic): DONE")
+                        sg.popup(
+                            seg(values["orthfile"], values["resdir"], value, False),
+                            title="results",
+                        )
 
                     except:
-                        sg.popup('ERROR: GSD not an integer value.')
+                        sg.popup("ERROR: GSD not an integer value.")
 
     # Running together
-    if event == 'Run All':
-        if values['imgdir'] == '':
-            sg.popup('ERROR: No image folder selected.', title='ERROR')
+    if event == "Run All":
+        if values["imgdir"] == "":
+            sg.popup("ERROR: No image folder selected.", title="ERROR")
         else:
-            if values['dwndir'] == '':
-                sg.popup('ERROR: No results folder selected.', title='ERROR')
+            if values["dwndir"] == "":
+                sg.popup("ERROR: No results folder selected.", title="ERROR")
             else:
                 try:
-                    masker(values['imgdir'], values['pb'])
-                    orthorec(values['imgdir'], values['dwndir'], values['q'], values['crop'], values['kmz'], values['ft'], False)
-                    value = float(calculate_gsd(values['dwndir'] + '/report.pdf'))
-                    sg.popup(seg(values['dwndir'] + '/odm_orthophoto.tif', values['dwndir'], value, False), title='results')
+                    masker(values["imgdir"], values["pb"])
+                    orthorec(
+                        values["imgdir"],
+                        values["dwndir"],
+                        values["q"],
+                        values["crop"],
+                        False,
+                        values["ft"],
+                        False,
+                    )
+                    value = float(calculate_gsd(values["dwndir"] + "/report.pdf"))
+                    sg.popup(
+                        seg(
+                            values["dwndir"] + "/odm_orthophoto.tif",
+                            values["dwndir"],
+                            value,
+                            False,
+                        ),
+                        title="results",
+                    )
 
                 except:
-                    sg.popup('ERROR: Problem processing request.')
-                
+                    sg.popup("ERROR: Problem processing request.")
+
 window.close()
--- main.py
+++ main.py
@@ -10,17 +10,20 @@ import PyPDF2
 import hakai_segmentation
 import rasterio
 import zipfile
-from PIL import Image 
+import black
+from PIL import Image
 from glint_mask_tools import RGBThresholdMasker
 from pyodm import Node, exceptions
 
-### ---------------------------- MASKER FUNCTION --------------------------- ###
-# This function takes an image directory and a pixel buffer and runs the 
-# glint-mask-tools python package.
-# :param imgdir: The directory where the images are stored.
-# :param pb: The value the glint-mask-tools package takes when calculating 
-#            pixels affected.
-### ------------------------------------------------------------------------ ###
+""" ------------------------ MASKER FUNCTION ---------------------------
+This function takes an image directory and a pixel buffer and runs the
+glint-mask-tools python package.
+:param imgdir: The directory where the images are stored.
+:param pb: The value the glint-mask-tools package takes when calculating
+           pixels affected.
+------------------------------------------------------------------------ """
+
+
 def masker(imgdir: str, pb: int):
     # This is the function that creates the image mask using glint-mask-tools.
     masker = RGBThresholdMasker(img_dir=imgdir, mask_dir=imgdir, pixel_buffer=pb)
@@ -29,50 +32,81 @@ def masker(imgdir: str, pb: int):
     # This turns the mask files from .png to .jpg
     for filename in os.listdir(imgdir):
         infilename = os.path.join(imgdir, filename)
-        if not os.path.isfile(infilename): continue
-        #oldbase = os.path.splitext(filename)
-        newname = infilename.replace('.png', '.JPG')
+        if not os.path.isfile(infilename):
+            continue
+        # oldbase = os.path.splitext(filename)
+        newname = infilename.replace(".png", ".JPG")
         os.rename(infilename, newname)
 
-### --------------------- ORTHORECTIFICATION FUNCTION ---------------------- ###
-# This function takes an image directory, a results directory, a quality value,
-# a crop value, a kmz boolean value and an exif boolean value. It takes all of
-# these values and runs the ODM (Open-Drone-Map) create_task function in a
-# docker container. https://docs.opendronemap.org/
-#
-# :param imgdir: The directory where the images are stored.
-#
-# :param dwndir: The directory where the results are stored.
-#
-# :param quality: ODM option to determine quality of the orthomosaic generated.
-#                 More info: https://docs.opendronemap.org/arguments/pc-quality/
-#
-# :param crop: ODM option to crop dataset boundaries. More info:
-#              https://docs.opendronemap.org/arguments/crop/
-#
-# :param kmz: ODM option to return Google Earth (kmz) file. More info:
-#             https://docs.opendronemap.org/arguments/orthophoto-kmz/
-#
-# :param ft: ODM option to determine feature types. More info:
-#            https://docs.opendronemap.org/arguments/feature-type/
-#
-# :param exif: ODM option to use EXIF information on images. More info:
-#              https://docs.opendronemap.org/arguments/use-exif/
-### ------------------------------------------------------------------------ ###
-def orthorec(imgdir: str, dwndir: str, quality: str, crop: int, kmz: bool, ft: str, exif: bool):
+
+""" --------------------- ORTHORECTIFICATION FUNCTION ---------------------- 
+This function takes an image directory, a results directory, a quality value,
+a crop value, a kmz boolean value and an exif boolean value. It takes all of
+these values and runs the ODM (Open-Drone-Map) create_task function in a
+docker container. https://docs.opendronemap.org/
+
+:param imgdir: The directory where the images are stored.
+
+:param dwndir: The directory where the results are stored.
+
+:param quality: ODM option to determine quality of the orthomosaic generated.
+                More info: https://docs.opendronemap.org/arguments/pc-quality/
+
+:param crop: ODM option to crop dataset boundaries. More info:
+             https://docs.opendronemap.org/arguments/crop/
+
+:param kmz: ODM option to return Google Earth (kmz) file. More info:
+            https://docs.opendronemap.org/arguments/orthophoto-kmz/
+
+:param ft: ODM option to determine feature types. More info:
+           https://docs.opendronemap.org/arguments/feature-type/
+
+:param exif: ODM option to use EXIF information on images. More info:
+             https://docs.opendronemap.org/arguments/use-exif/
+------------------------------------------------------------------------ """
+
+
+def orthorec(
+    imgdir: str, dwndir: str, quality: str, crop: int, kmz: bool, ft: str, exif: bool
+):
     imglist = []
 
     # Places each image filepath into the imglist list.
     for filenames in os.listdir(imgdir):
-        tmp = imgdir + '/' + filenames
+        tmp = imgdir + "/" + filenames
         imglist.append(tmp)
 
-    n = Node('localhost', 3000)
+    n = Node("localhost", 3000)
 
     try:
         # Start a task
         print("Uploading images...")
-        task = n.create_task(imglist, skip_post_processing=True, options={'orthophoto-cutline': False, 'min-num-features': 20000, 'pc-quality': quality, 'texturing-data-term': 'gmi', 'verbose' : True, 'debug': True, 'time': True, 'crop': crop, 'auto-boundary': False, 'skip-3dmodel': True, 'cog': False, 'use-3dmesh': False, 'pc-tile' : True, 'orthophoto-kmz': kmz, 'use-exif' : exif, 'orthophoto-resolution': 1, 'no-gpu': False, 'ignore-gsd': False, 'fast-orthophoto': True, 'feature-type': ft}) 
+        task = n.create_task(
+            imglist,
+            skip_post_processing=True,
+            options={
+                "orthophoto-cutline": False,
+                "min-num-features": 20000,
+                "pc-quality": quality,
+                "texturing-data-term": "gmi",
+                "verbose": True,
+                "debug": True,
+                "time": True,
+                "crop": crop,
+                "auto-boundary": False,
+                "skip-3dmodel": True,
+                "cog": False,
+                "use-3dmesh": False,
+                "pc-tile": True,
+                "orthophoto-kmz": kmz,
+                "use-exif": exif,
+                "orthophoto-resolution": 1,
+                "no-gpu": False,
+                "ignore-gsd": False,
+                "fast-orthophoto": True,
+                "feature-type": ft,
+            },
+        )
         print(task.info())
 
         try:
@@ -83,13 +117,13 @@ def orthorec(imgdir: str, dwndir: str, quality: str, crop: int, kmz: bool, ft: s
             print("Task completed, downloading results...")
 
             # Retrieve results
-            zipdir =  task.download_zip(dwndir)
-            with zipfile.ZipFile(zipdir, 'r') as zip_ref:
+            zipdir = task.download_zip(dwndir)
+            with zipfile.ZipFile(zipdir, "r") as zip_ref:
                 zip_ref.extractall(dwndir)
 
             # Removes mask files from the image folder.
             clean_masks(imgdir)
-            
+
             extract_essentials(dwndir, True)
 
         except exceptions.TaskFailedError as e:
@@ -99,160 +133,193 @@ def orthorec(imgdir: str, dwndir: str, quality: str, crop: int, kmz: bool, ft: s
     except exceptions.NodeResponseError as e:
         print("Error: %s" % e)
 
-### ---------------------- SEGMENTATION FUNCTION --------------------------- ###
-# This function takes an orthomosaic file, the path for a tif file,
-# a GSD (ground sampling distance) and a species classification boolean.
-# It takes the raster tif generated by the find_kelp function in the 
-# hakai_segmentation package and uses that to create a colormap file. This
-# colormap is a tif file that has 2 colors, one color designating kelp and one
-# color designating the lack of kelp. It then spits out area calculations.
-#
-# :param ortho: The path where the orthomosaic image is stored.
-# :param komp: The directory that the kelpomatic image will be placed in.
-# :param gsd: The ground sampling distance calculation.
-# NOT IMPLEMENTED:
-# :param spec: Wether kelpomatic should attempt to identify kelp species.
-### ------------------------------------------------------------------------ ###
+
+""" ---------------------- SEGMENTATION FUNCTION --------------------------- 
+This function takes an orthomosaic file, the path for a tif file,
+a GSD (ground sampling distance) and a species classification boolean.
+It takes the raster tif generated by the find_kelp function in the
+hakai_segmentation package and uses that to create a colormap file. This
+colormap is a tif file that has 2 colors, one color designating kelp and one
+color designating the lack of kelp. It then spits out area calculations.
+
+:param ortho: The path where the orthomosaic image is stored.
+:param komp: The directory that the kelpomatic image will be placed in.
+:param gsd: The ground sampling distance calculation.
+NOT IMPLEMENTED:
+:param spec: Wether kelpomatic should attempt to identify kelp species.
+------------------------------------------------------------------------ """
+
+
 def seg(ortho: str, komp: str, gsd: float, spec: bool):
     Image.MAX_IMAGE_PIXELS = 9999999999
     kelp = 0
     no_kelp = 0
     bull = 0
     giant = 0
-    
-    kom = komp + '/kelpomatic.tif'
-    
+
+    kom = komp + "/kelpomatic.tif"
+
     # Calling the kelpomatic tool
     hakai_segmentation.find_kelp(ortho, kom, species=spec, use_gpu=True)
-    
+
     # Writing the colormap
     with rasterio.Env():
-    
+
         with rasterio.open(kom) as src:
             shade = src.read(1)
             meta = src.meta
-    
-        with rasterio.open(komp + '/colormap.tif', 'w', **meta) as dst:
+
+        with rasterio.open(komp + "/colormap.tif", "w", **meta) as dst:
             dst.write(shade, indexes=1)
             dst.write_colormap(
-                1, {
+                1,
+                {
                     0: (0, 0, 255, 255),
                     1: (255, 255, 0, 255),
-                    #2: (0, 255, 0, 255),
-                    #3: (255, 0 , 0, 255)
-                    })
+                    # 2: (0, 255, 0, 255),
+                    # 3: (255, 0 , 0, 255)
+                },
+            )
             cmap = dst.colormap(1)
             # True
             assert cmap[0] == (0, 0, 255, 255)
             # True
             assert cmap[1] == (255, 255, 0, 255)
             ## True
-            #assert cmap[2] == (0, 255, 0, 255)
+            # assert cmap[2] == (0, 255, 0, 255)
             ## True
-            #assert cmap[3] == (255, 0 , 0, 255)
-    
+            # assert cmap[3] == (255, 0 , 0, 255)
+
     # Calculating the kelp pixels
-    with Image.open(komp + '/colormap.tif') as im:
-    
+    with Image.open(komp + "/colormap.tif") as im:
+
         for pixel in im.getdata():
-            if pixel == 0: # if your image is RGB (if RGBA, (0, 0, 0, 255) or so
+            if pixel == 0:  # if your image is RGB (if RGBA, (0, 0, 0, 255) or so
                 no_kelp += 1
             elif pixel == 1:
                 kelp += 1
-            #elif pixel == 2:
+            # elif pixel == 2:
             #    giant += 1
-            #elif pixel == 3:
+            # elif pixel == 3:
             #    bull += 1
-    
+
     # Calculating the area of the kelp in cm & acres
-    kcm = kelp*(gsd*gsd)
+    kcm = kelp * (gsd * gsd)
     kacres = kcm * 0.000000024711
-    
-    bcm = bull*(gsd*gsd)
+
+    bcm = bull * (gsd * gsd)
     bacres = bcm * 0.000000024711
-    
-    gcm = giant*(gsd*gsd)
+
+    gcm = giant * (gsd * gsd)
     gacres = gcm * 0.000000024711
-    
+
     # Printing the kelp surface area onto a .txt file
-    with open(komp + '/kelp_area.txt', 'a') as f:
-        f.write('kelp area cm^2: ' + str(kcm))
-        f.write('kelp area m^2: ' + str(kcm*.01))
-        f.write('\nkelp area acres: ' + str(kacres))
-        f.write('\nno kelp pixels=' + str(no_kelp)+', kelp pixels='+str(kelp))
-        #f.write('\nbull kelp pixels=' + str(bull)+', giant kelp pixels='+str(giant))
-        #f.write('\nbull kelp area acres=' + str(bacres)+', giant kelp acres='+str(gacres))
-    
+    with open(komp + "/kelp_area.txt", "a") as f:
+        f.write("kelp area cm^2: " + str(kcm))
+        f.write("kelp area m^2: " + str(kcm * 0.01))
+        f.write("\nkelp area acres: " + str(kacres))
+        f.write("\nno kelp pixels=" + str(no_kelp) + ", kelp pixels=" + str(kelp))
+        # f.write('\nbull kelp pixels=' + str(bull)+', giant kelp pixels='+str(giant))
+        # f.write('\nbull kelp area acres=' + str(bacres)+', giant kelp acres='+str(gacres))
+
     extract_essentials(komp, False)
-    
-    print('no kelp pixels=' + str(no_kelp)+', kelp pixels='+str(kelp))
-    print('kelp area (cm^2):' + str(kcm))
-    print('kelp area (acres):' + str(kacres))
 
-    return 'kelp area cm^2: ' + str(kcm) + '\nkelp area m^2: ' + str(kcm*.01) + '\nkelp area acres: ' + str(kacres) + '\nno kelp pixels=' + str(no_kelp)+', kelp pixels='+str(kelp)
+    print("no kelp pixels=" + str(no_kelp) + ", kelp pixels=" + str(kelp))
+    print("kelp area (cm^2):" + str(kcm))
+    print("kelp area (acres):" + str(kacres))
 
-### ---------------------------- CLEAN_MASK FUNCTION ----------------------- ###
-# This function takes an image directory and deletes all mask files located
-# within.
-# :param imgdir: The directory where the images are stored.
-### ------------------------------------------------------------------------ ###
+    return (
+        "kelp area cm^2: "
+        + str(kcm)
+        + "\nkelp area m^2: "
+        + str(kcm * 0.01)
+        + "\nkelp area acres: "
+        + str(kacres)
+        + "\nno kelp pixels="
+        + str(no_kelp)
+        + ", kelp pixels="
+        + str(kelp)
+    )
+
+
+""" ---------------------------- CLEAN_MASK FUNCTION -----------------------
+This function takes an image directory and deletes all mask files located
+within.
+:param imgdir: The directory where the images are stored.
+------------------------------------------------------------------------ """
+
+
 def clean_masks(imgdir: str):
     for filename in os.listdir(imgdir):
         infilename = os.path.join(imgdir, filename)
-        if not os.path.isfile(infilename): continue
-        if infilename.endswith('_mask.JPG'):
+        if not os.path.isfile(infilename):
+            continue
+        if infilename.endswith("_mask.JPG"):
             os.remove(infilename)
 
-### --------------------- EXTRACT_ESSENTIALS FUNCTION ---------------------- ###
-# This function takes a directory and deletes all files except for the ODM
-# report, the orthomosaic, the kmz file, the colormap and the kelp area report.
-# :param imgdir: The directory where the files are stored.
-# :param ext: Boolean value to determine if the report and orthophoto should be
-#             grabbed.
-### ------------------------------------------------------------------------ ###
+
+""" --------------------- EXTRACT_ESSENTIALS FUNCTION ----------------------
+This function takes a directory and deletes all files except for the ODM
+report, the orthomosaic, the kmz file, the colormap and the kelp area report.
+:param imgdir: The directory where the files are stored.
+:param ext: Boolean value to determine if the report and orthophoto should be
+            grabbed.
+------------------------------------------------------------------------ """
+
+
 def extract_essentials(dir: str, ext: bool):
     if ext == True:
-        os.replace(dir + '/odm_report/report.pdf', dir + '/report.pdf')
-        os.replace(dir + '/odm_orthophoto/odm_orthophoto.tif', dir + '/odm_orthophoto.tif')
+        os.replace(dir + "/odm_report/report.pdf", dir + "/report.pdf")
+        os.replace(
+            dir + "/odm_orthophoto/odm_orthophoto.tif", dir + "/odm_orthophoto.tif"
+        )
 
     for filename in os.listdir(dir):
         infilename = os.path.join(dir, filename)
-        if infilename.endswith('report.pdf') or infilename.endswith('odm_orthophoto.tif') or infilename.endswith('colormap.tif') or infilename.endswith('.kmz'):
-           continue 
-        elif infilename.endswith('.zip'):
+        if (
+            infilename.endswith("report.pdf")
+            or infilename.endswith("odm_orthophoto.tif")
+            or infilename.endswith("colormap.tif")
+            or infilename.endswith(".KML")
+        ):
+            continue
+        elif infilename.endswith(".zip"):
             os.remove(infilename)
-        elif infilename.endswith('.json'):
+        elif infilename.endswith(".json"):
             os.remove(infilename)
         elif os.path.isdir(infilename):
             shutil.rmtree(infilename)
         else:
             os.remove(infilename)
 
-### ------------------------- CALCULATE_GSD FUNCTION ----------------------- ###
-# This function takes the pdf report from ODM and scrapes the GSD value from it.
-# This solution is very janky, but this was the only way I could do what I
-# wanted to do.
-# :param pdfdir: The directory where the pdf is stored.
-### ------------------------------------------------------------------------ ###
+
+""" ------------------------- CALCULATE_GSD FUNCTION -----------------------
+This function takes the pdf report from ODM and scrapes the GSD value from it.
+This solution is very janky, but this was the only way I could do what I
+wanted to do.
+:param pdfdir: The directory where the pdf is stored.
+------------------------------------------------------------------------ """
+
+
 def calculate_gsd(pdfdir: str):
 
-    pdfFileObj = open(pdfdir, 'rb')
-    
+    pdfFileObj = open(pdfdir, "rb")
+
     # create a pdf reader object
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    
+
     # creating a page object
     pageObj = pdfReader.getPage(0)
-    
+
     # extracte text from page
     pdf = pageObj.extractText()
 
-    words = pdf.split(' ')
-    if '(GSD)' in words:
-        pos = words.index('(GSD)')
+    words = pdf.split(" ")
+    if "(GSD)" in words:
+        pos = words.index("(GSD)")
 
-    tmp = words[pos+1]
+    tmp = words[pos + 1]
     # closing the pdf file object
     pdfFileObj.close()
-    
+
     return tmp
