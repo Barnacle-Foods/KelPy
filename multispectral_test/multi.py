5e95d6293f4f23aaf035fd32028166f76b5220dd
Chet Russell <chet_dylan@outlook.com>
2023-06-29 09:39:53 -0800

    NDVI index calculation has been successful

--- multispectral_test/multi.py
+++ multispectral_test/multi.py
@@ -1,11 +1,62 @@
 import os
-from glob import glob
-import numpy as np
+
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
+import numpy as np
+import rioxarray as rxr
+import geopandas as gpd
 import earthpy as et
 import earthpy.spatial as es
 import earthpy.plot as ep
 
 # Get data and set your home working directory
-data = et.data.get_data("vignette-landsat")
+#data = et.data.get_data("vignette-landsat")
+
+#os.chdir(os.path.join(et.io.HOME, "earth-analytics"))
+
+data_path = "C:\\Users\\matt\\Documents\\GitHub\\New-ortho\\tlt\\code\\odm_orthophoto\\odm_orthophoto.tif"
+data = rxr.open_rasterio(data_path)
+
+# View shape of the data
+print(data.shape)
+
+#ep.plot_bands(data[0],
+#              title="RGB Imagery - Band 0",
+#              cbar=False)
+#plt.show()
+#
+#ep.plot_bands(data[1],
+#              title="RGB Imagery - Band 1",
+#              cbar=False)
+#plt.show()
+#
+#ep.plot_bands(data[2],
+#              title="RGB Imagery - Band 2",
+#              cbar=False)
+#plt.show()
+#
+#ep.plot_bands(data[3],
+#              title="RGB Imagery - Band 3",
+#              cbar=False)
+#plt.show()
+
+
+
+ep.plot_rgb(data.values,
+            rgb=[3, 0, 1],
+            title="RGB Composite image")
+plt.show()
+
+ndvi = es.normalized_diff(data[3], data[2])
+
+ep.plot_bands(ndvi,
+              cmap='PiYG',
+              scale=False,
+              vmin=-1, vmax=1,
+              title="NDVI (Normalized Distribution Vegetation Index)")
+plt.show()
+
+ep.hist(ndvi.values,
+        figsize=(12, 6),
+        title=["NDVI: Distribution of pixels"])
+
+plt.show()
--- multispectral_test/tmp.py
+++ multispectral_test/tmp.py
@@ -3,11 +3,11 @@ import os
 
 data = {
     "project_path": "C:\\Users\\matt\\Documents\\GitHub\\New-ortho\\tlt",
-    "orthophoto_cutline": True,
+    "orthophoto_cutline": False,
     #"min_num_features": 20000,
     #"dem_decimation": 50,
-    "pc_quality": "medium",
-    "feature_quality": "high",
+    "pc_quality": "low",
+    "feature_quality": "low",
     "texturing_data_term": "gmi",
     "verbose": True,
     "time": True,
@@ -24,12 +24,13 @@ data = {
     #"ignore_gsd": False,
     "fast_orthophoto": True,
     "feature-type": "sift",
-    "radiometric_calibration": "camera",
+    "radiometric_calibration": True,
     #"split": 500,
     #"split_overlap": 100,
     "optimize_disk_space": True,
     "resize-to": -1,
     "merge": "orthophoto",
+    #"primary_band": "NIR",
     } 
 
 yaml_output = yaml.dump(data, sort_keys=False) 
@@ -44,13 +45,13 @@ def write_yaml_to_file(py_obj, filename):
 write_yaml_to_file(data, 'C:\\Users\\matt\\Documents\\GitHub\\barnacle-imagery\\ODM\\settings')
 
     
-for file in os.listdir("C:\\Users\\matt\\Documents\\GitHub\\New-ortho\\tlt\\code\\images"):
-    filename = os.fsdecode(file)
-    #print(filename)
-    #print(filename[-5:])
-    if filename[-5:] == "3.tif" or filename[-5:] == "4.tif":
-        continue 
-    else:
-        os.remove("C:\\Users\\matt\\Documents\\GitHub\\New-ortho\\tlt\\code\\images\\" + filename)
+#for file in os.listdir("C:\\Users\\matt\\Documents\\GitHub\\New-ortho\\tlt\\code\\images"):
+#    filename = os.fsdecode(file)
+#    #print(filename)
+#    #print(filename[-5:])
+#    if filename[-5:] == "3.tif" or filename[-5:] == "4.tif":
+#        continue 
+#    else:
+#        os.remove("C:\\Users\\matt\\Documents\\GitHub\\New-ortho\\tlt\\code\\images\\" + filename)
 
 os.system('C:\\Users\\matt\\Documents\\GitHub\\barnacle-imagery\\ODM\\run.bat')
