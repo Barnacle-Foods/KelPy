7c24d42013600c1452ae2ba313d53b040fe8a8e9
Chet Russell <chet_dylan@outlook.com>
2023-07-11 13:02:59 -0800

    Adding unit tests in tester.py

---
+++ KelPy/tester.py
@@ -0,0 +1,27 @@
+# Kelpy unit tests
+# File created: 7/11/2023
+# Author: Chet Russell
+# Last edited: 7/11/2023 - Chet Russell
+
+import unittest
+import tempfile
+import shutil
+import os
+
+import core
+import gui
+
+import os
+cwd = os.getcwd()  # Get the current working directory (cwd)
+
+class UnitTests(unittest.TestCase):
+    def test_masker(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            shutil.copy(cwd + "\\test_imgs\\glint_test.JPG", tmpdirname)
+            core.masker(tmpdirname, 5)
+            with open(cwd + "\\test_imgs\\glint_test_mask.JPG","rb") as img:
+                with open(tmpdirname + "\\glint_test_mask.JPG","rb") as img_msk:
+                    self.assertEqual(img.read(), img_msk.read())
+
+if __name__=='__main__':
+	unittest.main()
