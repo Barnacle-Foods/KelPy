b36ee310dc29e0193ab5d6262585ac7e7301650d
Latseen <chet_dylan@outlook.com>
2023-07-28 16:51:52 -0700

    More GUI development

--- KelPy/qtgui.py
+++ KelPy/qtgui.py
@@ -10,6 +10,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+# Use a dict to sort the values needed?
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -18,43 +19,58 @@ class Ui_MainWindow(object):
         MainWindow.setMinimumSize(QtCore.QSize(313, 437))
         MainWindow.setMaximumSize(QtCore.QSize(313, 437))
         MainWindow.setDocumentMode(False)
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName("gridLayout")
+
         self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
         self.tabWidget.setObjectName("tabWidget")
+
         self.tab = QtWidgets.QWidget()
-        self.tab.setObjectName("tab")
+        self.tab.setObjectName("tab_0")
+
         self.imageFolder_label_0 = QtWidgets.QLabel(self.tab)
         self.imageFolder_label_0.setGeometry(QtCore.QRect(50, 10, 71, 16))
         self.imageFolder_label_0.setObjectName("imageFolder_label_0")
+
         self.downloadFolder_label_0 = QtWidgets.QLabel(self.tab)
         self.downloadFolder_label_0.setGeometry(QtCore.QRect(50, 50, 71, 16))
         self.downloadFolder_label_0.setObjectName("downloadFolder_label_0")
+
         self.imageFolder_select_0 = QtWidgets.QPushButton(self.tab)
         self.imageFolder_select_0.setGeometry(QtCore.QRect(140, 10, 75, 21))
-        self.imageFolder_select_0.setFlat(False)
         self.imageFolder_select_0.setObjectName("imageFolder_select_0")
+        self.imageFolder_select_0.clicked.connect(self.dir_select)
+
         self.downloadFolder_select_0 = QtWidgets.QPushButton(self.tab)
         self.downloadFolder_select_0.setGeometry(QtCore.QRect(140, 50, 75, 21))
         self.downloadFolder_select_0.setObjectName("downloadFolder_select_0")
+        self.downloadFolder_select_0.clicked.connect(self.dir_select)
+
         self.resultsFolder_name_label_0 = QtWidgets.QLabel(self.tab)
         self.resultsFolder_name_label_0.setGeometry(QtCore.QRect(20, 90, 101, 16))
         self.resultsFolder_name_label_0.setObjectName("resultsFolder_name_label_0")
+
         self.resultsFolder_name_0 = QtWidgets.QLineEdit(self.tab)
         self.resultsFolder_name_0.setGeometry(QtCore.QRect(140, 90, 113, 21))
         self.resultsFolder_name_0.setObjectName("resultsFolder_name_0")
+
         self.pixelBuff_label_0 = QtWidgets.QLabel(self.tab)
         self.pixelBuff_label_0.setGeometry(QtCore.QRect(60, 130, 61, 16))
         self.pixelBuff_label_0.setObjectName("pixelBuff_label_0")
+        
         self.pixelBuff_select_0 = QtWidgets.QSpinBox(self.tab)
         self.pixelBuff_select_0.setGeometry(QtCore.QRect(140, 130, 42, 21))
         self.pixelBuff_select_0.setMaximum(10)
         self.pixelBuff_select_0.setObjectName("pixelBuff_select_0")
+        
         self.quality_label_0 = QtWidgets.QLabel(self.tab)
         self.quality_label_0.setGeometry(QtCore.QRect(80, 170, 41, 16))
         self.quality_label_0.setObjectName("quality_label_0")
+        
         self.quality_select_0 = QtWidgets.QComboBox(self.tab)
         self.quality_select_0.setGeometry(QtCore.QRect(140, 170, 61, 21))
         self.quality_select_0.setEditable(False)
@@ -64,16 +80,20 @@ class Ui_MainWindow(object):
         self.quality_select_0.addItem("")
         self.quality_select_0.addItem("")
         self.quality_select_0.addItem("")
+        
         self.crop_label_0 = QtWidgets.QLabel(self.tab)
         self.crop_label_0.setGeometry(QtCore.QRect(90, 210, 31, 20))
         self.crop_label_0.setObjectName("crop_label_0")
+        
         self.crop_select_0 = QtWidgets.QSpinBox(self.tab)
         self.crop_select_0.setGeometry(QtCore.QRect(140, 210, 42, 21))
         self.crop_select_0.setMaximum(5)
         self.crop_select_0.setObjectName("crop_select_0")
+        
         self.specClass_label_0 = QtWidgets.QLabel(self.tab)
         self.specClass_label_0.setGeometry(QtCore.QRect(10, 250, 111, 20))
         self.specClass_label_0.setObjectName("specClass_label_0")
+        
         self.specClass_select_0 = QtWidgets.QComboBox(self.tab)
         self.specClass_select_0.setGeometry(QtCore.QRect(140, 250, 51, 22))
         self.specClass_select_0.setObjectName("specClass_select_0")
@@ -81,45 +101,59 @@ class Ui_MainWindow(object):
         self.specClass_select_0.addItem("")
         self.specClass_select_0.addItem("")
         self.specClass_select_0.setItemText(2, "")
+
         self.run_0 = QtWidgets.QPushButton(self.tab)
         self.run_0.setGeometry(QtCore.QRect(100, 290, 91, 31))
         self.run_0.setObjectName("run_0")
+        self.run_0.clicked.connect(self.run_main)
+
         self.progressBar_0 = QtWidgets.QProgressBar(self.tab)
         self.progressBar_0.setGeometry(QtCore.QRect(30, 340, 231, 23))
         self.progressBar_0.setProperty("value", 0)
         self.progressBar_0.setTextVisible(True)
         self.progressBar_0.setObjectName("progressBar_0")
+        
         self.tabWidget.addTab(self.tab, "")
         self.tab_2 = QtWidgets.QWidget()
-        self.tab_2.setObjectName("tab_2")
+        self.tab_2.setObjectName("tab_1")
+        
         self.imageFolder_label_1 = QtWidgets.QLabel(self.tab_2)
         self.imageFolder_label_1.setGeometry(QtCore.QRect(50, 10, 71, 16))
         self.imageFolder_label_1.setObjectName("imageFolder_label_1")
+        
         self.imageFolder_select_1 = QtWidgets.QPushButton(self.tab_2)
         self.imageFolder_select_1.setGeometry(QtCore.QRect(140, 10, 75, 21))
         self.imageFolder_select_1.setObjectName("imageFolder_select_1")
+        
         self.downloadFolder_label_1 = QtWidgets.QLabel(self.tab_2)
         self.downloadFolder_label_1.setGeometry(QtCore.QRect(50, 50, 71, 16))
         self.downloadFolder_label_1.setObjectName("downloadFolder_label_1")
+        
         self.downloadFolder_select_1 = QtWidgets.QPushButton(self.tab_2)
         self.downloadFolder_select_1.setGeometry(QtCore.QRect(140, 50, 75, 21))
         self.downloadFolder_select_1.setObjectName("downloadFolder_select_1")
+        
         self.resultsFolder_name_label_1 = QtWidgets.QLabel(self.tab_2)
         self.resultsFolder_name_label_1.setGeometry(QtCore.QRect(20, 90, 101, 16))
         self.resultsFolder_name_label_1.setObjectName("resultsFolder_name_label_1")
+        
         self.resultsFolder_name_1 = QtWidgets.QLineEdit(self.tab_2)
         self.resultsFolder_name_1.setGeometry(QtCore.QRect(140, 90, 113, 21))
         self.resultsFolder_name_1.setObjectName("resultsFolder_name_1")
+        
         self.pixelBuff_label_1 = QtWidgets.QLabel(self.tab_2)
         self.pixelBuff_label_1.setGeometry(QtCore.QRect(60, 130, 61, 16))
         self.pixelBuff_label_1.setObjectName("pixelBuff_label_1")
+        
         self.pixelBuff_select_1 = QtWidgets.QSpinBox(self.tab_2)
         self.pixelBuff_select_1.setGeometry(QtCore.QRect(140, 130, 42, 21))
         self.pixelBuff_select_1.setMaximum(10)
         self.pixelBuff_select_1.setObjectName("pixelBuff_select_1")
+        
         self.quality_label_1 = QtWidgets.QLabel(self.tab_2)
         self.quality_label_1.setGeometry(QtCore.QRect(80, 170, 41, 16))
         self.quality_label_1.setObjectName("quality_label_1")
+        
         self.quality_select_1 = QtWidgets.QComboBox(self.tab_2)
         self.quality_select_1.setGeometry(QtCore.QRect(140, 170, 61, 21))
         self.quality_select_1.setEditable(False)
@@ -129,9 +163,11 @@ class Ui_MainWindow(object):
         self.quality_select_1.addItem("")
         self.quality_select_1.addItem("")
         self.quality_select_1.addItem("")
+        
         self.featAlg_label = QtWidgets.QLabel(self.tab_2)
         self.featAlg_label.setGeometry(QtCore.QRect(30, 210, 91, 20))
         self.featAlg_label.setObjectName("featAlg_label")
+        
         self.featAlg_select = QtWidgets.QComboBox(self.tab_2)
         self.featAlg_select.setGeometry(QtCore.QRect(140, 210, 69, 22))
         self.featAlg_select.setObjectName("featAlg_select")
@@ -139,37 +175,48 @@ class Ui_MainWindow(object):
         self.featAlg_select.addItem("")
         self.featAlg_select.addItem("")
         self.featAlg_select.addItem("")
+        
         self.run_1 = QtWidgets.QPushButton(self.tab_2)
         self.run_1.setGeometry(QtCore.QRect(100, 290, 91, 31))
         self.run_1.setObjectName("run_1")
+        
         self.progressBar_1 = QtWidgets.QProgressBar(self.tab_2)
         self.progressBar_1.setGeometry(QtCore.QRect(30, 340, 231, 23))
         self.progressBar_1.setProperty("value", 0)
         self.progressBar_1.setObjectName("progressBar_1")
+        
         self.tabWidget.addTab(self.tab_2, "")
         self.tab_3 = QtWidgets.QWidget()
         self.tab_3.setObjectName("tab_3")
+        
         self.orthofile_label = QtWidgets.QLabel(self.tab_3)
         self.orthofile_label.setGeometry(QtCore.QRect(30, 10, 81, 16))
         self.orthofile_label.setObjectName("orthofile_label")
+        
         self.orthofile_select = QtWidgets.QPushButton(self.tab_3)
         self.orthofile_select.setGeometry(QtCore.QRect(140, 10, 75, 21))
         self.orthofile_select.setObjectName("orthofile_select")
+        
         self.resultsFolder_label_0 = QtWidgets.QLabel(self.tab_3)
         self.resultsFolder_label_0.setGeometry(QtCore.QRect(40, 50, 71, 16))
         self.resultsFolder_label_0.setObjectName("resultsFolder_label_0")
+        
         self.resultsFolder_select_0 = QtWidgets.QPushButton(self.tab_3)
         self.resultsFolder_select_0.setGeometry(QtCore.QRect(140, 50, 75, 21))
         self.resultsFolder_select_0.setObjectName("resultsFolder_select_0")
+        
         self.gsd_label = QtWidgets.QLabel(self.tab_3)
         self.gsd_label.setGeometry(QtCore.QRect(10, 90, 181, 16))
         self.gsd_label.setObjectName("gsd_label")
+        
         self.gsd_select = QtWidgets.QDoubleSpinBox(self.tab_3)
         self.gsd_select.setGeometry(QtCore.QRect(200, 90, 62, 16))
         self.gsd_select.setObjectName("gsd_select")
+        
         self.specClass_label_1 = QtWidgets.QLabel(self.tab_3)
         self.specClass_label_1.setGeometry(QtCore.QRect(10, 130, 111, 16))
         self.specClass_label_1.setObjectName("specClass_label_1")
+        
         self.specClass_select_1 = QtWidgets.QComboBox(self.tab_3)
         self.specClass_select_1.setGeometry(QtCore.QRect(140, 130, 51, 22))
         self.specClass_select_1.setObjectName("specClass_select_1")
@@ -177,47 +224,62 @@ class Ui_MainWindow(object):
         self.specClass_select_1.addItem("")
         self.specClass_select_1.addItem("")
         self.specClass_select_1.setItemText(2, "")
+        
         self.run_2 = QtWidgets.QPushButton(self.tab_3)
         self.run_2.setGeometry(QtCore.QRect(100, 290, 91, 31))
         self.run_2.setObjectName("run_2")
+        
         self.progressBar_2 = QtWidgets.QProgressBar(self.tab_3)
         self.progressBar_2.setGeometry(QtCore.QRect(30, 340, 231, 23))
         self.progressBar_2.setProperty("value", 0)
         self.progressBar_2.setObjectName("progressBar_2")
+        
         self.tabWidget.addTab(self.tab_3, "")
+        
         self.tab_4 = QtWidgets.QWidget()
         self.tab_4.setObjectName("tab_4")
+        
         self.ortho1_label = QtWidgets.QLabel(self.tab_4)
         self.ortho1_label.setGeometry(QtCore.QRect(40, 10, 81, 16))
         self.ortho1_label.setObjectName("ortho1_label")
+        
         self.ortho1_select = QtWidgets.QPushButton(self.tab_4)
         self.ortho1_select.setGeometry(QtCore.QRect(140, 10, 75, 21))
         self.ortho1_select.setObjectName("ortho1_select")
+        
         self.ortho2_label = QtWidgets.QLabel(self.tab_4)
         self.ortho2_label.setGeometry(QtCore.QRect(40, 50, 81, 16))
         self.ortho2_label.setObjectName("ortho2_label")
+        
         self.ortho2_select = QtWidgets.QPushButton(self.tab_4)
         self.ortho2_select.setGeometry(QtCore.QRect(140, 50, 75, 21))
         self.ortho2_select.setObjectName("ortho2_select")
+        
         self.resultsFolder_label_1 = QtWidgets.QLabel(self.tab_4)
         self.resultsFolder_label_1.setGeometry(QtCore.QRect(40, 90, 71, 16))
         self.resultsFolder_label_1.setObjectName("resultsFolder_label_1")
+        
         self.resultsFolder_select_1 = QtWidgets.QPushButton(self.tab_4)
         self.resultsFolder_select_1.setGeometry(QtCore.QRect(140, 90, 75, 21))
         self.resultsFolder_select_1.setObjectName("resultsFolder_select_1")
+        
         self.resultName = QtWidgets.QLineEdit(self.tab_4)
         self.resultName.setGeometry(QtCore.QRect(140, 140, 113, 21))
         self.resultName.setObjectName("resultName")
+        
         self.resultName_label = QtWidgets.QLabel(self.tab_4)
         self.resultName_label.setGeometry(QtCore.QRect(50, 140, 71, 16))
         self.resultName_label.setObjectName("resultName_label")
+        
         self.run3 = QtWidgets.QPushButton(self.tab_4)
         self.run3.setGeometry(QtCore.QRect(100, 290, 91, 31))
         self.run3.setObjectName("run3")
+        
         self.progressBar_3 = QtWidgets.QProgressBar(self.tab_4)
         self.progressBar_3.setGeometry(QtCore.QRect(30, 340, 231, 23))
         self.progressBar_3.setProperty("value", 0)
         self.progressBar_3.setObjectName("progressBar_3")
+        
         self.tabWidget.addTab(self.tab_4, "")
         self.gridLayout.addWidget(self.tabWidget, 0, 1, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
@@ -290,6 +352,20 @@ class Ui_MainWindow(object):
         self.resultName_label.setText(_translate("MainWindow", "Result Name:"))
         self.run3.setText(_translate("MainWindow", "Run"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_4), _translate("MainWindow", "Merge"))
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
+
+    def run_main(self):
+        print("Running...")
+        results = self.resultsFolder_name_0.text()
+
+    def dir_select(self):
+        self = str(QtWidgets.QFileDialog.getExistingDirectory(None, "Select Directory"))
+        print(self)
+
+    #def run0(self):
+
 
 
 if __name__ == "__main__":
